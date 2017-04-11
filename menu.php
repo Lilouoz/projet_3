4a11b648b8439ad89247d7b7b6df81b24f425e50
Lilouoz <honoreaurelie@hotmail.com>
2017-04-11 23:58:54 +0200

    menu.php
    creation du menu

---
+++ menu.php
@@ -0,0 +1,28 @@
+<div class="header">
+	<div class="container">
+		<div class="head">
+			<div class="navigation">
+				<span class="menu"></span>
+				<ul class="navig">
+					<li><a href="index.php" class="active">Accueil</a></li>
+					<li><a href="about.php">A propos</a></li>
+					<li><a href="contact.php">Contact</a></li>
+				</ul>
+			</div>
+			<div class="header-right">
+				<div class="search-bar">
+					<input type="text" value="Search" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Search';}">
+					<input type="submit" value="">
+				</div>
+				<ul>
+					<li><a href="#"><span class="fb"> </span></a></li>
+					<li><a href="#"><span class="twit"> </span></a></li>
+					<li><a href="#"><span class="pin"> </span></a></li>
+					<li><a href="#"><span class="rss"> </span></a></li>
+					<li><a href="#"><span class="drbl"> </span></a></li>
+				</ul>
+			</div>
+			<div class="clearfix"></div>
+		</div>
+	</div>
+</div>
