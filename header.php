0bb9776ef7683f5c966cfd89c205a6dfe323c1b8
Lilouoz <honoreaurelie@hotmail.com>
2017-04-12 00:05:38 +0200

    ajouter au header
    ajout du début du body n’ayant pas besoin d’etre dynamique

--- header.php
+++ header.php
@@ -1,15 +1,28 @@
 <!DOCTYPE html>
-	<html>
+<html>
 
-	<head>
-		<title>Coffee Break a Blog Category Flat Bootstarp responsive Website Template | Home :: w3layouts</title>
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<meta name="keywords" content="Coffee Break Responsive web template, Bootstrap Web Templates, Flat Web Templates, Andriod Compatible web template, 
+<head>
+	<title>Coffee Break a Blog Category Flat Bootstarp responsive Website Template | Home :: w3layouts</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="keywords" content="Coffee Break Responsive web template, Bootstrap Web Templates, Flat Web Templates, Andriod Compatible web template, 
 Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design" />
 
-		<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
-		<link href="css/style.css" rel='stylesheet' type='text/css' />
+	<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+	<link href="css/style.css" rel='stylesheet' type='text/css' />
 
-		<!--start-smoth-scrolling-->
-	</head>
+	<!--start-smoth-scrolling-->
+</head>
+
+
+
+<body>
+	<!--header-top-starts-->
+	<div class="header-top">
+		<div class="container">
+			<div class="head-main">
+				<a href="index.php"><img src="images/logo-1.png" alt="" /></a>
+			</div>
+		</div>
+	</div>
+	<!--header-top-end-->
