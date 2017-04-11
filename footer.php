c598b6bc0aaf0307ce91dee8447073924d67ec57
Lilouoz <honoreaurelie@hotmail.com>
2017-04-12 01:17:02 +0200

    include footer
    creation d’un include footer

---
+++ footer.php
@@ -0,0 +1,9 @@
+<!--footer-starts-->
+<div class="footer">
+	<div class="container">
+		<div class="footer-text">
+			<p>© 2017 Billet pour L'alaska. All Rights Reserved </p>
+		</div>
+	</div>
+</div>
+<!--footer-end-->
--- index.php
+++ index.php
@@ -246,78 +246,72 @@ $touslesbillets[]=$unarticle;
 							</div>
 						</div>
 						<!--slide-end-->
-						<!--footer-starts-->
-						<div class="footer">
-							<div class="container">
-								<div class="footer-text">
-									<p>© 2015 Coffee Break. All Rights Reserved | Design by <a href="http://w3layouts.com/" target="_blank">W3layouts</a> </p>
-								</div>
-							</div>
-						</div>
-						<!--footer-end-->
-						<!-- all script -->
+						<?php include_once("footer.php"); ?>
 
-						<script type="text/javascript">
-							$(window).load(function () {
 
-								$("#flexiselDemo3").flexisel({
-									visibleItems: 5,
-									animationSpeed: 1000,
-									autoPlay: true,
-									autoPlaySpeed: 3000,
-									pauseOnHover: true,
-									enableResponsiveBreakpoints: true,
-									responsiveBreakpoints: {
-										portrait: {
-											changePoint: 480,
-											visibleItems: 2
-										},
-										landscape: {
-											changePoint: 640,
-											visibleItems: 3
-										},
-										tablet: {
-											changePoint: 768,
-											visibleItems: 3
+							<!-- all script -->
+
+							<script type="text/javascript">
+								$(window).load(function () {
+
+									$("#flexiselDemo3").flexisel({
+										visibleItems: 5,
+										animationSpeed: 1000,
+										autoPlay: true,
+										autoPlaySpeed: 3000,
+										pauseOnHover: true,
+										enableResponsiveBreakpoints: true,
+										responsiveBreakpoints: {
+											portrait: {
+												changePoint: 480,
+												visibleItems: 2
+											},
+											landscape: {
+												changePoint: 640,
+												visibleItems: 3
+											},
+											tablet: {
+												changePoint: 768,
+												visibleItems: 3
+											}
 										}
-									}
-								});
+									});
 
-							});
-						</script>
-						<script type="text/javascript" src="js/jquery.flexisel.js"></script>
-						<script type="application/x-javascript">
-							addEventListener("load", function () {
-								setTimeout(hideURLbar, 0);
-							}, false);
-
-							function hideURLbar() {
-								window.scrollTo(0, 1);
-							}
-						</script>
-						<script src="js/jquery.min.js"></script>
-						<!---- start-smoth-scrolling---->
-						<script type="text/javascript" src="js/move-top.js"></script>
-						<script type="text/javascript" src="js/easing.js"></script>
-						<script type="text/javascript">
-							jQuery(document).ready(function ($) {
-								$(".scroll").click(function (event) {
-									event.preventDefault();
-									$('html,body').animate({
-										scrollTop: $(this.hash).offset().top
-									}, 1000);
 								});
-							});
-						</script>
-						<!-- script-for-menu -->
-						<!-- script-for-menu -->
-						<script>
-							$("span.menu").click(function () {
-								$(" ul.navig").slideToggle("slow", function () {});
-							});
-						</script>
-						<!-- script-for-menu -->
-						<!-- all script -->
+							</script>
+							<script type="text/javascript" src="js/jquery.flexisel.js"></script>
+							<script type="application/x-javascript">
+								addEventListener("load", function () {
+									setTimeout(hideURLbar, 0);
+								}, false);
+
+								function hideURLbar() {
+									window.scrollTo(0, 1);
+								}
+							</script>
+							<script src="js/jquery.min.js"></script>
+							<!---- start-smoth-scrolling---->
+							<script type="text/javascript" src="js/move-top.js"></script>
+							<script type="text/javascript" src="js/easing.js"></script>
+							<script type="text/javascript">
+								jQuery(document).ready(function ($) {
+									$(".scroll").click(function (event) {
+										event.preventDefault();
+										$('html,body').animate({
+											scrollTop: $(this.hash).offset().top
+										}, 1000);
+									});
+								});
+							</script>
+							<!-- script-for-menu -->
+							<!-- script-for-menu -->
+							<script>
+								$("span.menu").click(function () {
+									$(" ul.navig").slideToggle("slow", function () {});
+								});
+							</script>
+							<!-- script-for-menu -->
+							<!-- all script -->
 		</body>
 
 	</html>
