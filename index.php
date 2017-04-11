c857b3cac9cab49b5b2a95988d1fe0f7e685a8b5
Lilouoz <honoreaurelie@hotmail.com>
2017-04-11 23:42:10 +0200

    all script en bas de page
    all script en bas de page

--- index.php
+++ index.php
@@ -98,14 +98,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 				</div>
 			</div>
 		</div>
-		<!-- script-for-menu -->
-		<!-- script-for-menu -->
-		<script>
-			$("span.menu").click(function () {
-				$(" ul.navig").slideToggle("slow", function () {});
-			});
-		</script>
-		<!-- script-for-menu -->
+
 		<!--banner-starts-->
 		<div class="banner">
 			<div class="container">
@@ -323,60 +316,6 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 						</li>
 					</ul>
 
-					<script type="text/javascript">
-						$(window).load(function () {
-
-							$("#flexiselDemo3").flexisel({
-								visibleItems: 5,
-								animationSpeed: 1000,
-								autoPlay: true,
-								autoPlaySpeed: 3000,
-								pauseOnHover: true,
-								enableResponsiveBreakpoints: true,
-								responsiveBreakpoints: {
-									portrait: {
-										changePoint: 480,
-										visibleItems: 2
-									},
-									landscape: {
-										changePoint: 640,
-										visibleItems: 3
-									},
-									tablet: {
-										changePoint: 768,
-										visibleItems: 3
-									}
-								}
-							});
-
-						});
-					</script>
-					<script type="text/javascript" src="js/jquery.flexisel.js"></script>
-					<script type="application/x-javascript">
-						addEventListener("load", function () {
-							setTimeout(hideURLbar, 0);
-						}, false);
-
-						function hideURLbar() {
-							window.scrollTo(0, 1);
-						}
-					</script>
-					<script src="js/jquery.min.js"></script>
-					<!---- start-smoth-scrolling---->
-					<script type="text/javascript" src="js/move-top.js"></script>
-					<script type="text/javascript" src="js/easing.js"></script>
-					<script type="text/javascript">
-						jQuery(document).ready(function ($) {
-							$(".scroll").click(function (event) {
-								event.preventDefault();
-								$('html,body').animate({
-									scrollTop: $(this.hash).offset().top
-								}, 1000);
-							});
-						});
-					</script>
-
-
 					<div class="clearfix"> </div>
 				</div>
 			</div>
@@ -391,6 +330,69 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 			</div>
 		</div>
 		<!--footer-end-->
+		<!-- all script -->
+
+		<script type="text/javascript">
+			$(window).load(function () {
+
+				$("#flexiselDemo3").flexisel({
+					visibleItems: 5,
+					animationSpeed: 1000,
+					autoPlay: true,
+					autoPlaySpeed: 3000,
+					pauseOnHover: true,
+					enableResponsiveBreakpoints: true,
+					responsiveBreakpoints: {
+						portrait: {
+							changePoint: 480,
+							visibleItems: 2
+						},
+						landscape: {
+							changePoint: 640,
+							visibleItems: 3
+						},
+						tablet: {
+							changePoint: 768,
+							visibleItems: 3
+						}
+					}
+				});
+
+			});
+		</script>
+		<script type="text/javascript" src="js/jquery.flexisel.js"></script>
+		<script type="application/x-javascript">
+			addEventListener("load", function () {
+				setTimeout(hideURLbar, 0);
+			}, false);
+
+			function hideURLbar() {
+				window.scrollTo(0, 1);
+			}
+		</script>
+		<script src="js/jquery.min.js"></script>
+		<!---- start-smoth-scrolling---->
+		<script type="text/javascript" src="js/move-top.js"></script>
+		<script type="text/javascript" src="js/easing.js"></script>
+		<script type="text/javascript">
+			jQuery(document).ready(function ($) {
+				$(".scroll").click(function (event) {
+					event.preventDefault();
+					$('html,body').animate({
+						scrollTop: $(this.hash).offset().top
+					}, 1000);
+				});
+			});
+		</script>
+		<!-- script-for-menu -->
+		<!-- script-for-menu -->
+		<script>
+			$("span.menu").click(function () {
+				$(" ul.navig").slideToggle("slow", function () {});
+			});
+		</script>
+		<!-- script-for-menu -->
+		<!-- all script -->
 	</body>
 
 	</html>
