e5abcd9cd93c746c1d5a0dcd8b3f87d505cda0ba
Lilouoz <honoreaurelie@hotmail.com>
2017-04-12 01:20:51 +0200

    include script
    creation d’un include all script

--- index.php
+++ index.php
@@ -247,71 +247,71 @@ $touslesbillets[]=$unarticle;
 						</div>
 						<!--slide-end-->
 						<?php include_once("footer.php"); ?>
+							<?php include ("script.php"); ?>
 
+								<!-- all script -->
 
-							<!-- all script -->
+								<script type="text/javascript">
+									$(window).load(function () {
 
-							<script type="text/javascript">
-								$(window).load(function () {
-
-									$("#flexiselDemo3").flexisel({
-										visibleItems: 5,
-										animationSpeed: 1000,
-										autoPlay: true,
-										autoPlaySpeed: 3000,
-										pauseOnHover: true,
-										enableResponsiveBreakpoints: true,
-										responsiveBreakpoints: {
-											portrait: {
-												changePoint: 480,
-												visibleItems: 2
-											},
-											landscape: {
-												changePoint: 640,
-												visibleItems: 3
-											},
-											tablet: {
-												changePoint: 768,
-												visibleItems: 3
+										$("#flexiselDemo3").flexisel({
+											visibleItems: 5,
+											animationSpeed: 1000,
+											autoPlay: true,
+											autoPlaySpeed: 3000,
+											pauseOnHover: true,
+											enableResponsiveBreakpoints: true,
+											responsiveBreakpoints: {
+												portrait: {
+													changePoint: 480,
+													visibleItems: 2
+												},
+												landscape: {
+													changePoint: 640,
+													visibleItems: 3
+												},
+												tablet: {
+													changePoint: 768,
+													visibleItems: 3
+												}
 											}
-										}
-									});
+										});
 
-								});
-							</script>
-							<script type="text/javascript" src="js/jquery.flexisel.js"></script>
-							<script type="application/x-javascript">
-								addEventListener("load", function () {
-									setTimeout(hideURLbar, 0);
-								}, false);
-
-								function hideURLbar() {
-									window.scrollTo(0, 1);
-								}
-							</script>
-							<script src="js/jquery.min.js"></script>
-							<!---- start-smoth-scrolling---->
-							<script type="text/javascript" src="js/move-top.js"></script>
-							<script type="text/javascript" src="js/easing.js"></script>
-							<script type="text/javascript">
-								jQuery(document).ready(function ($) {
-									$(".scroll").click(function (event) {
-										event.preventDefault();
-										$('html,body').animate({
-											scrollTop: $(this.hash).offset().top
-										}, 1000);
 									});
-								});
-							</script>
-							<!-- script-for-menu -->
-							<!-- script-for-menu -->
-							<script>
-								$("span.menu").click(function () {
-									$(" ul.navig").slideToggle("slow", function () {});
-								});
-							</script>
-							<!-- script-for-menu -->
-							<!-- all script -->
+								</script>
+								<script type="text/javascript" src="js/jquery.flexisel.js"></script>
+								<script type="application/x-javascript">
+									addEventListener("load", function () {
+										setTimeout(hideURLbar, 0);
+									}, false);
+
+									function hideURLbar() {
+										window.scrollTo(0, 1);
+									}
+								</script>
+								<script src="js/jquery.min.js"></script>
+								<!---- start-smoth-scrolling---->
+								<script type="text/javascript" src="js/move-top.js"></script>
+								<script type="text/javascript" src="js/easing.js"></script>
+								<script type="text/javascript">
+									jQuery(document).ready(function ($) {
+										$(".scroll").click(function (event) {
+											event.preventDefault();
+											$('html,body').animate({
+												scrollTop: $(this.hash).offset().top
+											}, 1000);
+										});
+									});
+								</script>
+								<!-- script-for-menu -->
+								<!-- script-for-menu -->
+								<script>
+									$("span.menu").click(function () {
+										$(" ul.navig").slideToggle("slow", function () {});
+									});
+								</script>
+								<!-- script-for-menu -->
+								<!-- all script -->
 		</body>
 
 	</html>
