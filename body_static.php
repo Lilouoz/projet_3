5e49c5f8bbfc3aac335fd3b4a2b105a5dca841b2
Lilouoz <honoreaurelie@hotmail.com>
2017-04-12 00:52:49 +0200

    include body_static

---
+++ body_static.php
@@ -0,0 +1,9 @@
+<!--header-top-starts-->
+<div class="header-top">
+	<div class="container">
+		<div class="head-main">
+			<a href="index.php"><img src="images/logo-1.png" alt="" /></a>
+		</div>
+	</div>
+</div>
+<!--header-top-end-->
