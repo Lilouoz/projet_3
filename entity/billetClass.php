c88e9539994f618b1002897edc10f4cc2b68bfa9
Lilouoz <honoreaurelie@hotmail.com>
2017-04-19 18:35:41 +0200

    modif hydrate array
    verifier que la méthode existe

--- entity/billetClass.php
+++ entity/billetClass.php
@@ -12,7 +12,18 @@ class Billet
 // Un tableau de données doit être passé à la fonction (d'où le préfixe « array »).
 public function hydrate(array $donnees)
   {
- 
+ foreach ($donnees as $key => $value)
+  {
+    // On récupère le nom du setter correspondant à l'attribut.
+    $method = 'set'.ucfirst($key);
+        
+    // Si le setter correspondant existe.
+    if (method_exists($this, $method))
+    {
+      // On appelle le setter.
+      $this->$method($value);
+    }
+  }
   }
 
 	
