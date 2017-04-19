60f662d5f5d3874f9b6c0ae18d952e5acf4a2575
Lilouoz <honoreaurelie@hotmail.com>
2017-04-19 13:30:52 +0200

    chargées dynamiquement les classes
    spl autoloader

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php 
+init_set ('display_errors', 'on');
+error_reporting(E_ALL);
+
+function loadClass($class)
+{
+	include("entity/".$class.'Class.php');
+}
+
+spl_autoload_register('loadClass');
