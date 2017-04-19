d8c3ed5a4192306af876d692257863b89ef73e3a
Lilouoz <honoreaurelie@hotmail.com>
2017-04-19 17:20:28 +0200

    maj setters

--- entity/billetClass.php
+++ entity/billetClass.php
@@ -9,7 +9,7 @@ class Billet
 	private $_auteur;
 	private $_date_creation;
 		
-}
+
 // liste des getters
 
 public function id()
@@ -62,3 +62,60 @@ public function setId($id)
       $this->_id = $id;
     }
 }
+
+public function setImage($image)
+  {
+    if (is_string($image))
+   
+	{
+		$this->_image = $image;	
+	}
+  }
+
+
+
+public function setAlt($alt)
+  {
+    if (is_string($alt))
+   
+	{
+		$this->_alt = $alt;	
+	}
+  }
+
+public function setTitre($titre)
+  {
+    if (is_string($titre))
+   
+	{
+		$this->_titre = $titre;	
+	}
+  }
+
+public function setContenu($contenu)
+  {
+    if (is_string($contenu))
+   
+	{
+		$this->_contenu = $contenu;	
+	}
+  }
+
+public function setAuteur($auteur)
+  {
+    if (is_string($auteur))
+   
+	{
+		$this->_auteur = $auteur;	
+	}
+  }
+
+public function setDateCreation($date_creation)
+  {
+    if (is_string($date_creation))
+   
+	{
+		$this->_date_creation = $date_creation;	
+	}
+  }
+ }
