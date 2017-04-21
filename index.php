0d866b6a368ff594eb7ff6f03cc94a98bbfc5266
Lilouoz <honoreaurelie@hotmail.com>
2017-04-21 12:01:21 +0200

    Index syntax error reparé
    Index syntax error reparé

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php 
 //modele : Acces au donnes
 
-<<<<<<< HEAD
+ 
 try
 {
 	$bdd = new PDO('mysql:host=localhost;dbname=projet_blog_p3;charset=utf8', 'root', 'root');
@@ -20,13 +20,9 @@ $req->execute();
 while ($row = $req->fetch(PDO::FETCH_ASSOC)) {
 	
 	$touslesbillets[] = new Billet($row);
-	
-;?>
-=======
-require 'modele/blog/get_billets.php';
->>>>>>> parent of d3aeecf... retour en mode tableau
 
-$billets = get_billets();
+	}
+;?>
 
 
 
@@ -35,21 +31,28 @@ $billets = get_billets();
 
 	<!DOCTYPE html>
 	<html>
+
 	<!-- on récupere le headers  -->
-
-
 	<?php include_once("header.php"); ?>
-		<?php include_once("header.php"); ?> +
 
-			<body>
-				<?php include_once("header_top.php"); ?>
-					<?php include_once("menu.php"); ?>
-						<?php include_once("banner.php"); ?>
-							<?php include ("container_corps.php"); ?>
-								<?php include_once("footer.php"); ?>
-									<?php include ("script.php"); ?>
 
-			</body>
 
+		<body>
+
+
+			<?php include_once("header_top.php"); ?>
+
+				<?php include_once("menu.php"); ?>
+
+					<?php include_once("banner.php"); ?>
+
+						<?php include ("container_corps.php"); ?>
+
+							<?php include_once("footer.php"); ?>
+
+								<?php include ("script.php"); ?>
+
+
+		</body>
 
 	</html>
