c762eab3709e953c0ac31065865fd77d419ae68a
Lilouoz <honoreaurelie@hotmail.com>
2017-04-26 00:00:30 +0200

    change code error
     ini_set instead of init_set

--- config.php
+++ config.php
@@ -1,6 +1,10 @@
-<?php 
-init_set ('display_errors', 'on');
+<?php
+// show errors if not in php.ini
+
+ini_set ('display_errors', 'on');
 error_reporting(E_ALL);
+
+//autoload all class in entity folder
 
 function loadClass($class)
 {
@@ -8,4 +12,15 @@ function loadClass($class)
 }
 
 spl_autoload_register('loadClass');
-?>
+
+
+$host = 'http://'.$_SERVER['HTTP_HOST'].'/';
+$root =  $_SERVER['DOCUMENT_ROOT'].'/';
+
+// constant
+
+define("HOST", $host.'projet_blog_p3/' );
+define("ROOT", $root.'projet_blog_p3/' );
+
+define("PARTIAL", ROOT.'partial/');
+//define("ASSETS", HOST.'assets/');
