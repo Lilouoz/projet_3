2d7dc1b18720c69febc65b4ae5d314df118095ba
Lilouoz <honoreaurelie@hotmail.com>
2017-04-26 00:03:31 +0200

    set the manager
    getBillets
    Update
    Save
    Create
    byId

--- entity/billetsManagerClass.php
+++ entity/billetsManagerClass.php
@@ -1,58 +1,173 @@
 <?php
-class BilletsManager
+class BilletsManager extends DbManager
+
 {
-
-  private $_db; // Instance de PDO.
-
-  public function __construct($db)
-  {
-    $this->setDb($db);
-  }
-
-  public function add(Billet $billet)
-  { 
-    // Préparation de la requête d'insertion.
-    // Assignation des valeurs pour le  id, image , titre, contenu auteur date de creation du Billet.
-    // Exécution de la requête.
-	  $q=$this->_db->prepare('INSERT INTO `billets`(`id`, `image`, `alt`, `titre`, `contenu`, `auteur`, `date_creation`) VALUES ([value-1],[value-2],[value-3],[value-4],[value-5],[value-6],[value-7]) ');
-	 
-	  
-	  $q->bindValue(':id', $billet->id());
-      $q->bindValue(':image', $billet->image(), PDO::PARAM_STR);
-	  $q->bindValue(':alt', $billet->alt(), PDO::PARAM_STR);
-	  $q->bindValue(':titre', $billet->titre(), PDO::PARAM_STR);
-	  $q->bindValue(':contenu', $billet->contenu(), PDO::PARAM_STR);
-	  $q->bindValue(':auteur', $billet->auteur(), PDO::PARAM_STR);
-	  $q->bindValue(':date_creation', $billet->date_creation());
-	  $q->execute();
-		  
-		  
-  }
-
-  public function delete(Billet $billet)
-  {
-    // Exécute une requête de type DELETE.
-  }
-
-  public function get($id)
-  {
-    // Exécute une requête de type SELECT avec une clause WHERE, et retourne un objet Billet.
-  }
-
-  public function getList()
-  {
-    // Retourne la liste de tous les Billets.
-  }
-
-  public function update(Billet $billet)
-  {
-    // Prépare une requête de type UPDATE.
-    // Assignation des valeurs à la requête.
-    // Exécution de la requête.
-  }
-
-  public function setDb(PDO $db)
+	    /**
+     * return all billets in db
+     *
+     * @return array
+     */
+    public function getBillets() {
+        $db = $this->db;
+        $query = "SELECT * FROM billets";
+        $req = $db->prepare($query);
+        $req->execute();
+		
+        while($row = $req->fetch(PDO::FETCH_ASSOC)) 
+		
+		{
+            // instance of a billet object
+            $billet = new Billet();
+            // hydrate manualy from db datas
+            $billet->hydrate($row);
+            // now you have an array of object (instead of an array of array)
+            $billets[] = $billet;
+        }
+		
+		
+        return $billets;
+    }
+	
+    /**
+     * save the billet in db
+     *
+     * @param Billet $billet
+     */
+    public function persist(Billet $billet)
+    {
+        if($billet->getId() == null) {
+            $this->create($billet);
+        } else {
+            $this->update($billet);
+        }
+    }
+    /**
+     * update a billet in db
+     * @param Billet $billet
+     * @return $this
+     */
+	
+    public function update(Billet $billet) 
+	
+	{
+        $db = $this->db;
+        $req = $db->prepare("UPDATE `billets` SET `id`=[value-1],`image`=[value-2],`alt`=[value-3],`titre`=[value-4],`contenu`=[value-5],`auteur`=[value-6],`date_creation`=[value-7] WHERE 1");
+        $req->execute
+			
+			(
+			
+            array(
+                'image'       	=> $billet->getImage(),
+                'alt' 		  	=> $billet->getAlt(),
+				'titre'       	=> $billet->getTitre(),
+                'contenu' 	  	=> $billet->getContenu(),
+                'auteur'      	=> $billet->getAuteur(),
+                'date_creation' => $billet->getDate_creation(),
+                'id'          	=> $billet->getId()
+            )
+        );
+		
+		
+        return $this;
+    }
+	
+	
+    /**
+     * create a new billet in db
+     *
+     * @param Billet $billet
+     * @return $this
+     */
+    public function create(Billet $billet) 
+	
+	{
+        $db = $this->db;
+        $req = $db->prepare("INSERT INTO `billets`(`id`, `image`, `alt`, `titre`, `contenu`, `auteur`, `date_creation`) VALUES ([value-1],[value-2],[value-3],[value-4],[value-5],[value-6],[value-7])");
+        $req->execute
+			
+			(
+			
+            array(
+                'image'       	=> $billet->getImage(),
+                'alt' 		  	=> $billet->getAlt(),
+				'titre'       	=> $billet->getTitre(),
+                'contenu' 	  	=> $billet->getContenu(),
+                'auteur'      	=> $billet->getAuteur(),
+                'date_creation' => $billet->getDate_creation(),
+                'id'          	=> $billet->getId()
+            )
+        );
+		
+		
+        return $this;
+    }
+	
+	
+	/**
+	*
+	* get billet by Id
+	*
+	*@param $billet_id
+	*@return $billet
+	*/
+	
+    public function getBilletById($billet_id)
+    {
+        $db = $this->db;
+        $query = "SELECT * FROM billet WHERE id = ".$billet_id;
+        $req = $db->prepare($query);
+        $req->execute();
+		
+		
+        $row = $req->fetch(PDO::FETCH_ASSOC);
+		
+        $billet = new Billet();
+        $billet->hydrate($row);
+		
+		
+        return $billet;
+    }
+	
+	
+	/**
+     * delete a billet in db
+     *
+     * @param Billet $billet
+     * @return $this
+     */
+	
+	/* a ajouter apres connection
+    public function create(Billet $billet) 
+	
+	{
+        $db = $this->db;
+        $req = $db->prepare("DELETE FROM `billets` WHERE 1");
+        $req->execute
+			
+			(
+			
+            array(
+                'image'       	=> $billet->getImage(),
+                'alt' 		  	=> $billet->getAlt(),
+				'titre'       	=> $billet->getTitre(),
+                'contenu' 	  	=> $billet->getContenu(),
+                'auteur'      	=> $billet->getAuteur(),
+                'date_creation' => $billet->getDate_creation(),
+                'id'          	=> $billet->getId()
+            )
+        );
+		
+		
+        return $this;
+    }
+	
+	
+	public function setDb(PDO $db)
   {
     $this->_db = $db;
   }
+	
+	*/
+	
+	
 }
