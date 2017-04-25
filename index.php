a4059abfd9ca8a0cf1cb772425bdc7c67684b6cb
Lilouoz <honoreaurelie@hotmail.com>
2017-04-26 01:13:50 +0200

    remove stuff manage by manager

--- index.php
+++ index.php
@@ -7,53 +7,14 @@ include_once ('config.php');
 
 //modele : Acces au donnes
 
-
-/******** GERE PAR LE MANAGER ***********/
-try
-{
-	$db = new PDO('mysql:host=localhost;dbname=projet_blog_p3;charset=utf8', 'root', 'root');
-}
-catch (Exception $e)
-{
-        die('Erreur : ' . $e->getMessage());
-}
-$query = "SELECT * FROM billets ORDER BY date_creation";
-
-$req =$db->prepare($query);
-$req->execute();
-// PDO::FETCH_ASSOC = retourne un tableau indexé par le nom de la colonne comme retourné dans le jeu de résultats
-while ($row = $req->fetch(PDO::FETCH_ASSOC)) {
-	
-	$billet = array(
-		'id'=> $row['id'],
-		'image'=> $row['image'],
-		'alt'=> $row['alt'],
-		'titre'=> $row['titre'],
-		'contenu'=> $row['contenu'],
-		'auteur'=> $row['auteur'],
-		'date_creation'=> $row['date_creation'],
-	); 
-	//creation objet $billet 
- /*
-	{
-      $billet = new Billet($row)
-}
-    return $$billet
-	*/
- }
   
-  
-/*********** fin du manager **********/
-
-
-/* 
 
 
 $manager = new BilletsManager();
-$billets = $manager->getAll();
+$billets = $manager->getBillets();
 
 
-*/
+ 
 
 //traitement 
 //vue
